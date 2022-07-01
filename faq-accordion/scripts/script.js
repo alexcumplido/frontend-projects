3eb6571404e84ae06c707916f9239e9136f5f7d5
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-07-01 11:27:57 +0200

    Removed arrow img and place it via ::after

--- faq-accordion/scripts/script.js
+++ faq-accordion/scripts/script.js
@@ -1,19 +1,16 @@
 const accordionItems = document.querySelectorAll('.accordion__item');
 const accordionTitles = document.querySelectorAll(".accordion-title");
 const accordionText = document.querySelectorAll('.accordion-text');
-const arrows = document.querySelectorAll('.arrow');
 let currentItem = null;
 
 function displayItem(i) {
-    accordionTitles[i].classList.add("title-active");
+    accordionTitles[i].classList.add("accordion-title-active");
     accordionText[i].classList.add("accordion-text-show");
-    arrows[i].classList.add("arrow-rotate");
 }
 
 function hideItem(i) {
-    accordionTitles[i].classList.remove("title-active");
+    accordionTitles[i].classList.remove("accordion-title-active");
     accordionText[i].classList.remove("accordion-text-show");
-    arrows[i].classList.remove("arrow-rotate");
 }
 
 for (let i = 0; i < accordionItems.length; i++) {
