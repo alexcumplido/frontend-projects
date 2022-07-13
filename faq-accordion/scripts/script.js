924bc60053e716ac8ae2c64bdab26035ec446c96
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-07-13 16:02:18 +0200

    Remove display hide functions2

--- faq-accordion/scripts/script.js
+++ faq-accordion/scripts/script.js
@@ -1,25 +1,17 @@
 const accordionItems = document.querySelectorAll('.accordion__item');
 let currentItem = null;
 
-function displayItem(i) {
-    accordionItems[i].classList.add('accordion_item--active');
-}
-
-function hideItem(i) {
-    accordionItems[i].classList.remove('accordion_item--active');
-}
-
 for (let i = 0; i < accordionItems.length; i++) {
     accordionItems[i].addEventListener(('click'), function () {
         if (currentItem === null) {
-            displayItem(i);
+            accordionItems[i].classList.add('accordion__item--active');
             currentItem = i;
         } else if (currentItem === i) {
-            hideItem(currentItem);
+            accordionItems[i].classList.remove('accordion__item--active');
             currentItem = null;
         } else {
-            hideItem(currentItem);
-            displayItem(i);
+            accordionItems[currentItem].classList.remove('accordion__item--active');
+            accordionItems[i].classList.add('accordion__item--active');
             currentItem = i;
         }
     });
