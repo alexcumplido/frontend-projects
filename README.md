8590033628ae214fe2088cacb3974ba611ea76c1
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-07-24 12:28:54 +0200

    Refactored showProfileData

--- README.md
+++ README.md
@@ -3,11 +3,10 @@
 In this repository I develop different projects from the Frontend Mentor plattform using frontend techonologies.
 
 ## Projects
-  - [GitHub user search app  (Work in progress)](https://alexcumplido.github.io/frontend-mentor/github-search-api/)
+  - [GitHub user search app  (Work in progress)](https://alexcumplido.github.io/frontend-mentor/github-user-api/)
   - [Time Tracking Dashboard](https://alexcumplido.github.io/frontend-mentor/time-dashboard/) 
   - [Advice generator app](https://alexcumplido.github.io/frontend-mentor/adviceAPI-generator)
   - [Intro section with dropdown navigation](https://alexcumplido.github.io/frontend-mentor/dropdown-navigation)
-
   - [Expenses chart component](https://alexcumplido.github.io/frontend-mentor/bar-chart)
   - [Intro component with sign-up form](https://alexcumplido.github.io/frontend-mentor/form-validation)
   - [FAQ accordion card](https://alexcumplido.github.io/frontend-mentor/faq-accordion)
@@ -17,10 +16,15 @@ In this repository I develop different projects from the Frontend Mentor plattfo
 
 <!--
 To do
+- [Four card feature section](https://www.frontendmentor.io/challenges/four-card-feature-section-weK1eFYK)
+- [Skilled e-learning landing Page](https://www.frontendmentor.io/challenges/skilled-elearning-landing-page-S1ObDrZ8q)
+
 - [Pricing component toggle](https://www.frontendmentor.io/challenges/pricing-component-with-toggle-8vPwRMIC)
 - [Tip calculator](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX)
+
 - [Crowdfunding product page](https://www.frontendmentor.io/challenges/crowdfunding-product-page-7uvcZe7ZR)
 - [Interactive price component](https://www.frontendmentor.io/challenges/interactive-pricing-component-t0m8PIyY8)
+
 - [Officelite coming soon site](https://www.frontendmentor.io/challenges/officelite-coming-soon-site-M4DIPNz8g)
 - [Price card toggle](https://www.frontendmentor.io/challenges/pricing-component-with-toggle-8vPwRMIC)
 
--- github-user-api/scripts/script.js
+++ github-user-api/scripts/script.js
@@ -1,78 +1,33 @@
-/*
-1. Understand the problem 
-
-    We have a small component with an input field and a button. On loading the component a network request is already executed and that information will be displayed into the DOM via DOM traversing. Once the user enters a username we insert it into an URL as query parameter to init a network request to GitHub API. If the user exist we receive a response that we have to proces and obtain certain values from that response to inyect into our DOM Model via DOM traversing. if the user does not exist we have to processt anyway and show an error to the user.
-
-    Inputs:
-        Username to search as string inserted by the user
-        An event on the click button to start the search of the username
-        Response from the network fetching petition
-
-    Output:
-        A response from the network petition, can be ok or can be failed
-        If response ok an object with key properties and values
-        If response ok but user not found an object with key and values
-        A series of values inyected into the DOM via DOM Traversing
-
-    Explicit requirements ?
-        A input string to init the fetch, field can't be empty
-        A response from the network resquest to fill the DOM Model via traversing
-
-    Implicit requirements?
-        
-2. Create an example (features, user-flow, edge cases) 
-    The user inputs a username and certain values from the response are displayed via DOM traversing
-    The user inputs a username and the response returns non found so an error have to ve displayed via DOM traversing
-    The response from the network request return null or undefined values so special values needs to be inyected via DOM traversing for those fields
-    
-3. Determine data structure
-    Destructuring the object storing a copy of certain values into variables
-     
-4. Write an algorithm (in pseudocode)
-
-    - On first load, fetch the GitHub api and show the profile information for Octocat https://api.github.com/users/octocat
-    - When user inputs fhe search field and clicks the submit button:
-
-        - Prevent default behaviour from form submitting
-        - Init a fetch remplacing :username by the user input https://api.github.com/users/:username
-        - Display an error message if the user does not exist in the GitHub API
-        - If the user exist destructure the object response and get required key and corresponding values.
-        - Store those values into variables
-
-            - If a GitHub user hasn't added their name. Use their username (login) as name without the '@' and again with the '@' as username.
-                - Add an '@' symbol to the username
-            - If a GiHub user's bio is empty reference in that variable the string "This profile has no bio"
-            - If any location, website, twitter or company are empty store in that variable the string "Not Available". Website, twitter and company must be links that point to those resources.
-            - For the company link, it should remove the `@` symbol and link to the company page on GitHub.For Octocat, with `@github` being returned for the company, this would lead to a URL of`https://github.com/github`.
-
-5. Code
-    Follow algorithm and test as you go
-
-What is the smallest unit of work / problem unit that I can work on ?
-    How can I get it done and fast ?
-        If stuck: how can I zoom out ?
-*/
-
 let URL_API = `https://api.github.com/users/:username`;
-let dataFetched;
 const formSearch = document.querySelector('.form__input__search');
 const formSubmit = document.querySelector('.form__submit');
 const formError = document.querySelector('.form__error');
+const profileAvatar = document.querySelector('.profile-avatar');
+const nameField = document.querySelector('.name');
+const userField = document.querySelector('.user');
+const loginField = document.querySelector('.login');
+const createdField = document.querySelector('.created_at');
+const bioField = document.querySelector('.bio');
+const reposField = document.querySelector('.public_repos');
+const followersField = document.querySelector('.followers');
+const followingField = document.querySelector('.following');
+const locationField = document.querySelector('.location');
+const blogField = document.querySelector('.blog');
+const twitterField = document.querySelector('.twitter');
+const companyField = document.querySelector('.company');
 
 formSubmit.addEventListener('click', function (event) {
     event.preventDefault();
-    fetchData(URL_API);
+    if (formSearch.value) fetchData(URL_API);
 })
 
 function fetchData(URL_API) {
-    let githubUser = formSearch.value || 'octopus';
+    let githubUser = formSearch.value.trim() || 'octocat';
     URL_API = URL_API.replace(':username', githubUser);
-
     fetch(URL_API)
         .then(function (promise) {
             return promise.text();
         }).then(function (responseToParse) {
-            console.log(responseToParse);
             return JSON.parse(responseToParse);
         }).then(function (responseParsed) {
             validateUser(responseParsed);
@@ -87,10 +42,44 @@ function validateUser(dataFetched) {
         formError.classList.add('show');
     } else {
         formError.classList.remove('show');
-        displayProfile(dataFetched);
+        showProfileData(dataFetched);
     }
 }
-function displayProfile(dataFetched) {
-    let { login, name, company, blog, location, bio, twitter_username, public_repos, followers, following, created_at } = dataFetched;
-    console.log(login, name, company, blog, location, bio, twitter_username, public_repos, followers, following, created_at);
+function showProfileData(dataFetched) {
+    let linksNull = `Not Available`;
+    let bioNull = `This profile has no bio`;
+
+    let {
+        login, avatar_url, html_url, name, company, blog, location, bio, twitter_username, public_repos, followers, following, created_at
+    } = dataFetched;
+
+    profileAvatar.setAttribute('src', avatar_url);
+    userField.setAttribute('href', html_url);
+    nameField.textContent = name || login;
+    userField.textContent = `@${login}`;
+
+    let joinDate = new Date(created_at);
+    let day = joinDate.getDay();
+    let month = joinDate.toLocaleString('default', { month: 'short' });
+    let year = joinDate.getFullYear();
+    createdField.textContent = `Joined ${day} ${month} ${year}`
+
+    bioField.textContent = bio || bioNull;
+
+    reposField.textContent = public_repos;
+    followingField.textContent = following;
+    followersField.textContent = followers;
+
+    locationField.textContent = location || linksNull;
+
+    blogField.textContent = blog || linksNull;
+    if (blog) blogField.setAttribute('href', `${blog}`);
+
+    twitterField.textContent = (twitter_username) ? `@${twitter_username}` : linksNull;
+    if (twitter_username) twitterField.setAttribute('href', `https://twitter.com/${twitter_username}`);
+
+    companyField.textContent = company || linksNull;
+    if (company) companyField.setAttribute('href', `https://github.com/${company}`);
 }
+
+fetchData(URL_API);
