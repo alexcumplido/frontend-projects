2565720585a42b9f711feaf7a5fafef8161eed08
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-07-28 10:21:20 +0200

    Layout desktop view

--- github-user-api/scripts/script.js
+++ github-user-api/scripts/script.js
@@ -6,11 +6,11 @@ const themeToggle = document.querySelector('.theme__toggle');
 const formSearch = document.querySelector('.form__input');
 const formSubmit = document.querySelector('.form__submit');
 const formError = document.querySelector('.form__error-hide');
-const profileAvatar = document.querySelector('.profile__user__avatar');
-const basicInfo = document.querySelector('.profile__user-info')
-const profileBio = document.querySelector('.bio');
-const statsItems = document.querySelectorAll('.profile__stats-item');
-const socialLinks = document.querySelectorAll('.profile__link');
+const userAvatar = document.querySelector('.profile__user__avatar');
+const userBasic = document.querySelector('.profile__user-info')
+const userBio = document.querySelector('.bio');
+const userStats = document.querySelectorAll('.profile__stats-item');
+const userSocial = document.querySelectorAll('.profile__link');
 
 function getInputSearch() {
     return formSearch.value.trim() || 'octocat';
@@ -46,36 +46,36 @@ function showProfileData(dataFetched) {
     let month = joinDate.toLocaleString('default', { month: 'short' });
     let year = joinDate.getFullYear();
 
-    profileAvatar.setAttribute('src', avatar_url);
+    userAvatar.setAttribute('src', avatar_url);
 
-    basicInfo.children[0].textContent = name || login;
-    basicInfo.children[1].firstChild.textContent = `@${login}`;
-    basicInfo.children[1].firstChild.setAttribute('href', html_url);
-    basicInfo.children[2].textContent = `Joined ${day} ${month} ${year}`;
+    userBasic.children[0].textContent = name || login;
+    userBasic.children[1].firstChild.textContent = `@${login}`;
+    userBasic.children[1].firstChild.setAttribute('href', html_url);
+    userBasic.children[2].textContent = `Joined ${day} ${month} ${year}`;
 
-    profileBio.textContent = bio || `This profile has no bio`;
+    userBio.textContent = bio || `This profile has no bio`;
 
-    statsItems[0].children[1].innerText = public_repos;
-    statsItems[1].children[1].innerText = following;
-    statsItems[2].children[1].innerText = followers;
+    userStats[0].children[1].innerText = public_repos;
+    userStats[1].children[1].innerText = following;
+    userStats[2].children[1].innerText = followers;
 
-    socialLinks[0].children[1].textContent = location || `Not Available`;
-    socialLinks[1].children[1].textContent = blog || `Not Available`;
-    socialLinks[2].children[1].textContent = (twitter_username) ? `@${twitter_username}` : `Not Available`;
-    socialLinks[3].children[1].textContent = company || `Not Available`;
+    userSocial[0].children[1].textContent = location || `Not Available`;
+    userSocial[1].children[1].textContent = blog || `Not Available`;
+    userSocial[2].children[1].textContent = (twitter_username) ? `@${twitter_username}` : `Not Available`;
+    userSocial[3].children[1].textContent = company || `Not Available`;
 
-    if (blog) socialLinks[1].children[1].setAttribute('href', blog);
-    if (twitter_username) socialLinks[2].children[1].setAttribute('href', `https://twitter.com/${twitter_username}`);
-    if (company) socialLinks[3].children[1].setAttribute('href', `https://github.com/${company}`);
+    if (blog) userSocial[1].children[1].setAttribute('href', blog);
+    if (twitter_username) userSocial[2].children[1].setAttribute('href', `https://twitter.com/${twitter_username}`);
+    if (company) userSocial[3].children[1].setAttribute('href', `https://github.com/${company}`);
 
-    for (let i = 0; i < socialLinks.length; i++) {
-        socialLinks[i].children[1].classList.remove('link-not-avaible')
+    for (let i = 0; i < userSocial.length; i++) {
+        userSocial[i].children[1].classList.remove('link-not-avaible')
     }
 
-    if (!location) socialLinks[0].children[1].classList.add('link-not-avaible');
-    if (!blog) socialLinks[1].children[1].classList.add('link-not-avaible');
-    if (!twitter_username) socialLinks[2].children[1].classList.add('link-not-avaible');
-    if (!company) socialLinks[3].children[1].classList.add('link-not-avaible');
+    if (!location) userSocial[0].children[1].classList.add('link-not-avaible');
+    if (!blog) userSocial[1].children[1].classList.add('link-not-avaible');
+    if (!twitter_username) userSocial[2].children[1].classList.add('link-not-avaible');
+    if (!company) userSocial[3].children[1].classList.add('link-not-avaible');
 }
 
 formSubmit.addEventListener('click', function (event) {
