4effc57e582b1e1290c16287ebc7bd56e72ff86e
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-08-04 12:12:22 +0200

    Refactored render functions

--- interactive-pricing/scripts/script.js
+++ interactive-pricing/scripts/script.js
@@ -1,60 +1,60 @@
 const pageViews = document.querySelector('.card__views');
-const pageInputRange = document.querySelector('.card__input');
-const pageCost = document.querySelector('.card__cost');
-const costToggle = document.querySelector('.button__toggle');
+const inputRange = document.querySelector('.card__input');
+const pageCost = document.querySelector('.card__price');
+const buttonToggle = document.querySelector('.btn__toggle');
 const cardDiscount = document.querySelector('.card__discount');
 let finalPrice = 16;
 let flushPrice;
 
-window.addEventListener('DOMContentLoaded', function (event) {
-    pageInputRange.value = 100000;
-    cardDiscount.textContent = "25%";
-    printViewsAndCost(pageInputRange.value);
+function renderInputSlider() {
+    inputRange.style.backgroundSize =
+        (inputRange.value - inputRange.min) * 100 / (inputRange.max - inputRange.min) + '% 100%';
+}
 
-    pageInputRange.style.backgroundSize =
-        (pageInputRange.value - pageInputRange.min) * 100 / (pageInputRange.max - pageInputRange.min) + '% 100%';
-})
+function renderCost() {
+    pageCost.textContent = `$${finalPrice.toFixed(2)}`;
+}
 
-function printViewsAndCost(views) {
+function renderViews(views) {
     if (views >= 10000 && views < 100000) views = `${views.substring(0, 2)}K`
     if (views >= 100000 && views < 1000000) views = `${views.substring(0, 3)}K`
     if (views >= 1000000) views = `${views.substring(0, 1)}M`
     pageViews.textContent = `${views} pageviews`;
-    pageCost.textContent = `$${finalPrice.toFixed(2)}`;
-    const span = document.createElement('span');
-    span.textContent = '/ month';
-    pageCost.appendChild(span);
 }
 
-pageInputRange.addEventListener('input', function (event) {
-    event.target.style.backgroundSize =
-        (event.target.value - event.target.min) * 100 / (event.target.max - event.target.min) + '% 100%';
+window.addEventListener('DOMContentLoaded', function () {
+    inputRange.value = 100000;
+    cardDiscount.textContent = "25%";
+    renderViews(inputRange.value);
+    renderCost();
+    renderInputSlider();
+});
+
+inputRange.addEventListener('input', function (event) {
     let views = event.target.value;
     if (views >= 10000 && views < 50000) finalPrice = 8;
     if (views >= 50000 && views <= 100000) finalPrice = 12;
     if (views >= 100000 && views < 500000) finalPrice = 16;
     if (views >= 500000 && views < 1000000) finalPrice = 24;
     if (views >= 1000000) finalPrice = 36;
-    printViewsAndCost(event.target.value)
+    renderViews(event.target.value)
+    renderCost();
+    renderInputSlider();
 });
 
-costToggle.addEventListener('click', function (event) {
+buttonToggle.addEventListener('click', function (event) {
     if (event.target.ariaPressed === 'false') {
         event.target.ariaPressed = true;
         flushPrice = finalPrice;
         finalPrice = finalPrice - (finalPrice * 0.25);
         cardDiscount.textContent = "-25%";
-        printViewsAndCost(pageInputRange.value);
-        event.target.classList.replace(
-            'button__toggle--monthly', 'button__toggle--yearly'
-        );
+        event.target.classList.replace('btn__toggle--month', 'btn__toggle--year');
+        renderCost();
     } else if (event.target.ariaPressed === 'true') {
         event.target.ariaPressed = false;
         finalPrice = flushPrice;
-        printViewsAndCost(pageInputRange.value)
         cardDiscount.textContent = "25%";
-        event.target.classList.replace(
-            'button__toggle--yearly', 'button__toggle--monthly'
-        );
+        event.target.classList.replace('btn__toggle--year', 'btn__toggle--month');
+        renderCost();
     }
 });
