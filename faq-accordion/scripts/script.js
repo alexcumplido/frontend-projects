8051b15a44445b765454cb6a21ec87a9ba8fae8b
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-08-06 18:06:59 +0200

    Implemented accessibility markup and refactor JS logic according to aria-expanded

--- faq-accordion/scripts/script.js
+++ faq-accordion/scripts/script.js
@@ -1,18 +1,11 @@
 const accordionItems = document.querySelectorAll('.accordion__item');
-let currentItem = null;
 
-for (let i = 0; i < accordionItems.length; i++) {
-    accordionItems[i].addEventListener(('click'), function () {
-        if (currentItem === null) {
-            accordionItems[i].classList.add('accordion__item--active');
-            currentItem = i;
-        } else if (currentItem === i) {
-            accordionItems[i].classList.remove('accordion__item--active');
-            currentItem = null;
-        } else {
-            accordionItems[currentItem].classList.remove('accordion__item--active');
-            accordionItems[i].classList.add('accordion__item--active');
-            currentItem = i;
+accordionItems.forEach(function (element, index, list) {
+    element.addEventListener(('click'), function (event) {
+        if (element.ariaExpanded === 'false') {
+            element.setAttribute('aria-expanded', 'true');
+        } else if (element.ariaExpanded === 'true') {
+            element.setAttribute('aria-expanded', 'false');
         }
     });
-}
+})
