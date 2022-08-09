79f6dea7e05fa53ffe3b198ff83bcb5fec5d7d3a
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-08-09 17:41:33 +0200

    Project setup

---
+++ tip-calculator/README.md
@@ -0,0 +1,41 @@
+User Story 
+
+As a `type of user`, I want `some goal` so that `some reason`
+
+As a ...
+I want to  ...
+So that ... 
+
+Conditions of satisfaction
+
+
+PEDAC
+1. Problem
+    (inputs-outputs, explicit-implicit requirements)
+
+2. Examples
+    (user-flow features, edge cases)
+
+3. Data Structure
+
+4. Algorightm 
+    (pseudocode)
+
+5. Code 
+    (test)
+
+1. What is the overall structure ?
+2. What is the crossplatform design ? Mayor shifts ?
+3. What elements I need to use ?
+4. What are the main mediaQueries ?
+5. What are the equivalences of units between mobile, tablet and desktop ?
+
+What is the smallest unit of work/problem unit that I can work on ?
+How can I get it done and fast ?
+If stuck : how can I zoom out ?
+
+ACTIVE RECALL
+    Understand concept
+    Explain yourself (Why and how) ?
+    Check what is wrong
+    Verbalize to Sir Duck.
---
+++ tip-calculator/scripts/script.js
@@ -0,0 +1,89 @@
+// PEDAC
+// 1. Problem
+//     (inputs - outputs, explicit - implicit requirements)
+
+// 2. Examples
+//     (user - flow features, edge cases)
+
+// 3. Data Structure
+
+// 4. Algorightm
+//     (pseudocode)
+
+// 5. Code
+//     (test)
+
+
+// What is the smallest unit of work / problem unit that I can work on ?
+//     How can I get it done and fast ?
+//         If stuck: how can I zoom out ?
+
+{
+    /*
+
+    var main = document.querySelector('main');
+
+    var currentlyLightTheme = window.matchMedia("(prefers-color-scheme:light)")
+
+    function toggle() { 
+
+        if (currentlyLightTheme) {
+            main.className = "overrideLight";
+            currentlyLightTheme = false;
+        } else {
+            main.className = "overrideDark";
+            currentlyLightTheme = true;
+        }
+    }
+
+*/
+}
+
+/*
+
+var main = document.querySelector('main');
+
+var currentlyLightTheme = window.matchMedia("(prefers-color-scheme:light)")
+
+function toggle() {
+
+    if (currentlyLightTheme) {
+        main.className = "overrideLight";
+        currentlyLightTheme = false;
+    } else {
+        main.className = "overrideDark";
+        currentlyLightTheme = true;
+    }
+}
+
+*/
+
+
+/*
+.override-light {
+    bakcground-color: white;
+    color: black;
+}
+
+.override-dark {
+    background-color: black;
+    color: white;
+}
+
+*/
+
+
+// const body = document.querySelector('body');
+// var currentlyLightTheme = window.matchMedia(
+//     "(prefers-color-scheme: light)"
+// );
+
+// function toggleTheme() {
+//     if (currentlyLightTheme) {
+//         body.classList.add('override-light');
+//         currentlyLightTheme = false;
+//     } else {
+//         body.classList.add('override-dardk');
+//         currentlyLightTheme = true;
+//     }
+// }
